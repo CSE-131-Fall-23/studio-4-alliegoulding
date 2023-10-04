db055973b4f67d88663fcb81d8fdf0d202233e4f
amybeanblossom <amybeanblossom@localhost>
2023-10-04 13:49:50 -0500

    hi

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(0.5, 0.5, .3, .2);
 		
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledCircle(.5, .5, .1);
+		
+		StdDraw.setPenColor(Color.CYAN);
+		StdDraw.ellipse(.5, .5, .1, .03);
+		
+		StdDraw.setPenColor(Color.CYAN);
+		StdDraw.ellipse(.5, .5, .03, .1);
 	}
 }
